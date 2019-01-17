76e7f7966b97c8aabc8d67dfd4512c0baf263e01
sgunawan228 <46772400+sgunawan228@users.noreply.github.com>
2019-01-17 14:29:43 +0700

    Add files via upload

---
+++ s1.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="../index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="../news.php">News <span class="sr-only">(current)</span></a>
+      </li>
+	        <li class="nav-item">
+        <a class="nav-link" href="../form.php">Buy</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Registrasi
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="../login.php">Log In</a>
+          <a class="dropdown-item" href="#">Sign Up</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br />
+<div class="table-responsive">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../img/Untitled-3.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/cover2.jpg" alt="Second slide">
+    </div>
+	<div class="carousel-item">
+      <img class="d-block w-100" src="../img/cover3.png" alt="3 slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<br />
+
+<table style="width:100%"  >
+	<tr>
+	<td colspan="2"><strong>Spesifikasi</strong></td>
+	</tr>
+	<tr>
+		<td>Prosesor</td>
+		<td>Intel® Core™ i7 6700HQ Processor,M</td>
+	</tr>
+	<tr>
+		<td>Sistem Operasi</td>
+		<td>Windows 10 Home</td>
+	</tr>
+	<tr>
+		<td>Memory</td>
+		<td>DDR4 2133(O.C.) MHz SDRAM, 2 x DIMM socket for expansion up to 8 GB SDRAM </td>
+	</tr>
+	<tr>
+		<td>Display</td>
+		<td>15.6" 16:9 IPS UHD (3840 x 2160)</td>
+	</tr>
+	<tr>
+		<td>Grafis</td>
+		<td>NVIDIA® GeForce® GTX 960M dengan 4GB GDDR5 VRAM</td>
+	</tr>
+	<tr>
+		<td>Storage</td>
+		<td>2.5" SATA3- 1TB HDD 7200 RPM</td>
+	</tr>
+	<tr>
+		<td>Drive Optik</td>
+		<td>Super-Multi DVD</td>
+	</tr>
+	<tr>
+		<td>Card Reader</td>
+		<td>3 -in-1 card reader ( SD/ MS Pro/ MMC)</td>
+	</tr>
+	<tr>
+		<td>Kamera</td>
+		<td>Super-Multi DVD</td>
+	</tr>
+	<tr>
+		<td>HD Web Camera</td>
+		<td>Networking</td>
+	</tr>
+	<tr>
+		<td>Networking</td>
+		<td>Terintegrasi 802.11 a/b/g/n/ac atau 802.11 ac (WiDi)10/100/1000/Gigabits Base T</td>
+	</tr>
+	<tr>
+		<td>Interface</td>
+		<td>1 x Microphone-in jack <br>
+		1 x Headphone-out jack (1 x SPDIF)<br>
+		3 x USB 3.0 port(s) <br>
+		1 x RJ45 LAN Jack for LAN insert<br> 
+		1 x HDMI <br>
+		1 x mini Display Port</td> 
+	</tr>
+	<tr>
+		<td>Audio</td>
+		<td>Built-in 2 Speaker(s) And Microphone<br>
+			ICEpower®<br>
+			ASUS SonicMaster Premium Technology</td>
+	</tr>
+	<tr>
+		<td>Baterai</td>
+		<td>5200 mAh 56 Whrs</td>
+	</tr>
+	<tr>
+		<td>Adaptor Daya</td>
+		<td>Output : 19 V DC, 6.3 A, 120 W</td>
+	</tr>
+	<tr>
+		<td>Dimensi</td>
+		<td>383 x 255 x 28 ~31.5 cm (WxDxH)</td>
+	</tr>
+	<tr>
+		<td>Berat</td>
+		<td>2.7 kg</td>
+	</tr>
+	<tr>
+		<td>Sekuritas</td>
+		<td>Kensington lock<br>
+			BIOS Booting User Password Protection<br>
+			HDD User Password Protection and Security</td>
+	</tr>
+	<tr>
+		<td>Certificates</td>
+		<td>UL, CE Marking Compliance, BSMI, CCC, CB, EPEAT, EU Flower, Energy star, CECP, WEEE, Erp 2013, RoHS, JATE</td>
+	</tr>
+	<tr>
+		<td>Jaminan</td>
+		<td>2 tahun garansi hardware global. *berbeda di setiap negara</td>
+	</tr>
+	<tr>
+		<td>Catatan</td>
+		<td>*Audio: ROG Audio Wizard</td>
+	</tr>
+</table>
+</div>
+</div>
+</body>
+<br>
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Footer links -->
+      <div class="row text-center text-md-left mt-3 pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Ketahui Lebih Lanjut</h6>
+          <p>Banyak orang mengatakan bahwa kamu tidak bisa meningkatkan kesempurnaan. Namun dengan Produk kami yang baru ini, 
+		  kami telah membuktikan bahwa mereka salah karena kami berhasil melakukan peningkatan signifikan dan membuatnya jauh lebih baik dari generasi sebelumnya.</p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none">
+
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Butuh Bantuan</h6>
+          <p>
+            <a href="#!">Hubungi Kami</a>
+          </p>
+          <p>
+            <a href="#!">Registrasi Produk</a>
+          </p>
+          <p>
+            <a href="#!">Download Product Guide</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+        </div>
+
+        <!-- Grid column -->
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i class="fa fa-home mr-3"></i> Semarang, Jawa Tengah</p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i> info@gmail.com</p>
+          <p>
+            <i class="fa fa-phone mr-3"></i> +62 8968 880 9038</p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Footer links -->
+
+      <hr>
+
+      <!-- Grid row -->
+      <div class="row d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8">
+
+          <!--Copyright-->
+          <p class="text-center text-md-left">© 2018 Copyright:
+            <a href="#">
+              <strong>Pemrograman Berbasis Web</strong>
+            </a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+
+          <!-- Social buttons -->
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-google-plus"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+  </footer>
+  <!-- Footer -->
+</html>
---
+++ s5.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="../index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="../news.php">News <span class="sr-only">(current)</span></a>
+      </li>
+	        <li class="nav-item">
+        <a class="nav-link" href="../form.php">Buy</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Registrasi
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="../login.php">Log In</a>
+          <a class="dropdown-item" href="#">Sign Up</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br />
+<div class="table-responsive">
+<br />
+<div class="table-responsive">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../img/Untitled-3.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/cover2.jpg" alt="Second slide">
+    </div>
+	<div class="carousel-item">
+      <img class="d-block w-100" src="../img/cover3.png" alt="Second slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<br />
+<table style="width:100%"  >
+	<tr>
+	<td colspan="2"><strong>Spesifikasi</strong></td>
+	</tr>
+	<tr>
+		<td>Prosesor</td>
+		<td>Intel® Core™ i5 7200U Processor</td>
+	</tr>
+	<tr>
+		<td>Sistem Operasi</td>
+		<td>Windows 10 Home<br>
+			DOS
+		
+		</td>
+	</tr>
+	<tr>
+		<td>Memory</td>
+		<td>up to 4 GB SDRAM</td>
+	</tr>
+	<tr>
+		<td>Display</td>
+		<td>14.0" (16:9) LED backlit HD (1366x768) Glare 60Hz Panel with 45% NTSC</td>
+	</tr>
+	<tr>
+		<td>Grafis</td>
+		<td>NVIDIA GeForce 930MX , with 2GB GDDR3 VRAM</td>
+	</tr>
+	<tr>
+		<td>Storage</td>
+		<td>1TB 5400RPM SSH</td>
+	</tr>
+	<tr>
+		<td>Drive Optik</td>
+		<td>Super-Multi DVD</td>
+	</tr>
+	<tr>
+		<td>Card Reader</td>
+		<td>3 -in-1 card reader ( SD/ MS Pro/ MMC)</td>
+	</tr>
+	<tr>
+		<td>Kamera</td>
+		<td>Super-Multi DVD</td>
+	</tr>
+	<tr>
+		<td>HD Web Camera</td>
+		<td>Networking</td>
+	</tr>
+	<tr>
+		<td>Networking</td>
+		<td>Integrated 802.11b/g/n <br>
+			10/100/1000 Base T <br>
+			<b>Bluetooth</b><br>
+			Built-in Bluetooth V4.0
+		</td>
+	</tr>
+	<tr>
+		<td>Interface</td>
+		<td>1 x COMBO audio jack  <br>
+		1 x VGA port/Mini D-sub 15-pin for external monitor<br>
+		1 x Type A USB3.0 (USB3.1 GEN1) <br>
+		0 x USB 3.0 port(s)<br>
+		1 x USB 2.0 port(s)<br>
+		1 x RJ45 LAN Jack for LAN insert <br>
+		1 x HDMI </td> 
+	</tr>
+	<tr>
+		<td>Audio</td>
+		<td>ASUS SonicMaster Technology</td>
+	</tr>
+	<tr>
+		<td>Baterai</td>
+		<td>2 Cells 37 Whrs Polymer Battery</td>
+	</tr>
+	<tr>
+		<td>Adaptor Daya</td>
+		<td>Plug Type :ø4 (mm)<br>
+			Output :19 V DC, 3.42 A, 65 W<br>
+			Input :100 -240 V AC, 50/60 Hz universal 3/ 2 pin compact power supply system</td>
+	</tr>
+	<tr>
+		<td>Dimensi</td>
+		<td>34.8 x 24.28 x 2.53 cm (WxDxH)</td>
+	</tr>
+	<tr>
+		<td>Berat</td>
+		<td>2.1,2.1 kg ~ 2.1,2.1 kg with Battery</td>
+	</tr>
+	<tr>
+		<td>Sekuritas</td>
+		<td>Kensington lock<br>
+			BIOS Booting User Password Protection<br>
+			HDD User Password Protection and Security</td>
+	</tr>
+	<tr>
+		<td>Jaminan</td>
+		<td>2 tahun garansi hardware global. *berbeda di setiap negara</td>
+	</tr>
+</table>
+</div>
+</div>
+</body>
+<br>
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Footer links -->
+      <div class="row text-center text-md-left mt-3 pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Ketahui Lebih Lanjut</h6>
+          <p>Banyak orang mengatakan bahwa kamu tidak bisa meningkatkan kesempurnaan. Namun dengan Produk kami yang baru ini, 
+		  kami telah membuktikan bahwa mereka salah karena kami berhasil melakukan peningkatan signifikan dan membuatnya jauh lebih baik dari generasi sebelumnya.</p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none">
+
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Butuh Bantuan</h6>
+          <p>
+            <a href="#!">Hubungi Kami</a>
+          </p>
+          <p>
+            <a href="#!">Registrasi Produk</a>
+          </p>
+          <p>
+            <a href="#!">Download Product Guide</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+        </div>
+
+        <!-- Grid column -->
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i class="fa fa-home mr-3"></i> Semarang, Jawa Tengah</p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i> info@gmail.com</p>
+          <p>
+            <i class="fa fa-phone mr-3"></i> +62 8968 880 9038</p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Footer links -->
+
+      <hr>
+
+      <!-- Grid row -->
+      <div class="row d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8">
+
+          <!--Copyright-->
+          <p class="text-center text-md-left">© 2018 Copyright:
+            <a href="#">
+              <strong>Pemrograman Berbasis Web</strong>
+            </a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+
+          <!-- Social buttons -->
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-google-plus"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+  </footer>
+  <!-- Footer -->
+</html>
